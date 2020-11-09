3b0a8a18490436255a2cb833963dd8737fa2615d
KevinAviles28 <bejarano.a.kevin@gmail.com>
2020-11-09 12:08:29 -0300

    Agregue 3 archivos JS

---
+++ ciclos.js
@@ -0,0 +1,6 @@
+let ciclos = 0;
+let i;
+for (let i = 0; i < 10; i++) {
+    ciclos+=1;
+    console.log(ciclos)
+}
---
+++ variable.js
@@ -0,0 +1,7 @@
+let variable1= 5;
+let variable2= "variable2"
+let variable3= false;
+
+console.log("tipo de variable 1 "+variable1);
+console.log("tipo de variable 2 "+variable2);
+console.log("tipo de variable 3 "+variable3);
